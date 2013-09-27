0db851577c5abb8241aeb1860c8288dad178e1a4
Yaqi Zhang <hsbzyq@gmail.com>
2013-09-26 21:44:54 -0400

    Create Project

---
+++ FinancialManager/FinancialManager/FinancialManagerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FinancialManagerAppDelegate.h
+//  FinancialManager
+//
+//  Created by  mac on 9/26/13.
+//  Copyright (c) 2013  mac. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FinancialManagerAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FinancialManager/FinancialManager/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FinancialManager
+//
+//  Created by  mac on 9/26/13.
+//  Copyright (c) 2013  mac. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
