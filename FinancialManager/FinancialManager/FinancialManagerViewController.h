dcac7a1555e5b88d28117e5b93da146209ec4be7
Yaqi Zhang <hsbzyq@gmail.com>
2013-09-27 01:11:43 -0400

    New ios project

---
+++ FinancialManager/FinancialManager/FinancialManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  FinancialManagerViewController.h
+//  FinancialManager
+//
+//  Created by  mac on 9/27/13.
+//  Copyright (c) 2013  mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FinancialManagerViewController : UIViewController
+
+@end
---
+++ FinancialManager/FinancialManager/FinancialManagerViewController.m
@@ -0,0 +1,29 @@
+//
+//  FinancialManagerViewController.m
+//  FinancialManager
+//
+//  Created by  mac on 9/27/13.
+//  Copyright (c) 2013  mac. All rights reserved.
+//
+
+#import "FinancialManagerViewController.h"
+
+@interface FinancialManagerViewController ()
+
+@end
+
+@implementation FinancialManagerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
